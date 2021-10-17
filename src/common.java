d139ad7763d936e903c23d365f1c8ff08d50f9bc
trh0030 <trh0030@uah.edu>
2021-10-17 16:55:50 -0500

    Fix default completed processes value

--- src/common.java
+++ src/common.java
@@ -7,6 +7,6 @@ package src;
 
 public class common {
     public static boolean CPU1RUNNING = false;
-    public static int completedProcesses = 20;
+    public static int completedProcesses = 0;
     //public static int totalTime = 0;
 }
